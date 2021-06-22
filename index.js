6ccde64f86bd05298ae326e9866a0477746a26d5
DxxxxY <introsdream@gmail.com>
2021-06-22 17:50:43 -0400

    Cleanup, Comments

--- index.js
+++ index.js
@@ -22,32 +22,41 @@ app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`)
 })
 
+//Mappings map
 const fieldsMap = [];
 const methodsMap = [];
 const paramsMap = [];
 
-const deobfuscate = async(obfuscated, version, res) => {
+//Deobfuscate
+const deobfuscate = (obfuscated, version, res) => {
+    //Parse fields
     fs.createReadStream(`mappings/${version}/fields.csv`)
         .pipe(csv())
         .on('data', (data) => { fieldsMap.push(data) })
         .on('end', () => {
+            //Parse methods
             fs.createReadStream(`mappings/${version}/methods.csv`)
                 .pipe(csv())
                 .on('data', (data) => { methodsMap.push(data) })
                 .on('end', () => {
+                    //Parse params
                     fs.createReadStream(`mappings/${version}/params.csv`)
                         .pipe(csv())
                         .on('data', (data) => { paramsMap.push(data) })
                         .on('end', () => {
+                            //Deobfuscate fields
                             fieldsMap.forEach(field => {
                                 obfuscated = obfuscated.replace(new RegExp(field.searge, "g"), field.name)
                             })
+                            //Deobfuscate methods
                             methodsMap.forEach(method => {
                                 obfuscated = obfuscated.replace(new RegExp(method.searge, "g"), method.name)
                             })
+                            //Deobfuscate params
                             paramsMap.forEach(param => {
                                 obfuscated = obfuscated.replace(new RegExp(param.param, "g"), param.name)
                             })
+                            //Send deobfuscated code with whitespace
                             res.send(`<pre>${obfuscated}</pre>`)
                         })
                 })
